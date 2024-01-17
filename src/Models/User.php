cde390a9cc5a476f02bfaec56473dfba1bf68bb1
Nicolas DENIS <me@scoopandrun.ovh>
2024-01-17 10:31:00 +0100

    Add post archive page
    Refactored models to use getters and setters.

--- src/Models/User.php
+++ src/Models/User.php
@@ -4,15 +4,87 @@ namespace App\Models;
 
 use App\Core\DateTime;
 
-class User
+class User implements \Stringable
 {
-    public function __construct(
-        public int $id = 0,
-        public string $name = "Anonyme",
-        public string $email = "",
-        public string $password = "",
-        public bool $isAdmin = false,
-        public ?DateTime $createdAt = null,
-    ) {
+    private int $id;
+    private string $name = "Anonyme";
+    private string $email = "";
+    private string $password = "";
+    private bool $isAdmin = false;
+    private ?DateTime $createdAt = null;
+
+    public function __construct()
+    {
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password)
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    public function getAdmin()
+    {
+        return $this->isAdmin;
+    }
+
+    public function setAdmin(bool $isAdmin)
+    {
+        $this->isAdmin = $isAdmin;
+        return $this;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->name;
     }
 }
