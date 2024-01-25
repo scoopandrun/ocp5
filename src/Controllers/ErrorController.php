cf8785d0d8dbac37e5ec9043823d2f7513f48da0
Nicolas DENIS <me@scoopandrun.ovh>
2024-01-25 17:03:35 +0100

    Add HTTP response class
    Added an HTTPReponse class to have better control over the HTTP responses.

--- src/Controllers/ErrorController.php
+++ src/Controllers/ErrorController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controllers;
 
+use App\Core\Exceptions\AppException;
+
 class ErrorController extends Controller
 {
-    public function __construct(protected \Throwable $e)
+    public function __construct(protected AppException $e)
     {
         parent::__construct();
     }
 
     public function show(): void
     {
-        $this->twig->display("front/error.html.twig", [
-            "error" => $this->e
-        ]);
+        $this->response->setCode($this->e->httpStatus);
+
+        // HTML response
+        if ($this->request->acceptsHTML()) {
+            $this->response
+                ->setHTML($this->twig->render("front/error.html.twig", [
+                    "error" => $this->e
+                ]))
+                ->send();
+        }
+
+        // JSON response
+        if ($this->request->acceptsJSON()) {
+            $json = json_encode([
+                "message" => $this->e->getMessage(),
+            ]);
+
+            $this->response
+                ->setJSON($json)
+                ->send();
+        }
+
+        // Default response
+        $this->response->setText($this->e->getMessage())->send();
     }
 }
