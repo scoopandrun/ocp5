0927200445d21efe1641ecd09cb93ca4332adc3a
Nicolas DENIS <me@scoopandrun.ovh>
2024-02-06 13:42:18 +0100

    Add minimum password length

--- src/Core/Security.php
+++ src/Core/Security.php
@@ -6,6 +6,8 @@ class Security
 {
     const LOGIN_FAILURE_TIMEOUT = 2;
 
+    const MINIMUM_PASSWORD_LENGTH = 12;
+
     /**
      * Delay before sending the response after a failed login attempt.
      */
